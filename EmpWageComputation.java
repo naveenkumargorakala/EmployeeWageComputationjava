3cc99d045ee03bb6e8f56de3764d8752f1e12136
Naveen <naveengorakala814@gmail.com>
2023-03-05 17:39:10 -0800

    Add:Employee DailyWage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,14 +1,19 @@
 import java.util.*;
-public class EmpWageComputation
+public class EmpWage
 {
    public static void main(String args[])
-   {
+   {  
+      int DailyWage,WagePhr=20,empHrs;
       System.out.println("Welcome to Employee Wage Computation Program");
        Random rand = new Random();
        int Attendance= rand.nextInt(2);
        if(Attendance==1)
        {
           System.out.println("Employee is Present");
+          empHrs=8;
+          System.out.println("Employee is Present");
+          DailyWage=WagePhr*empHrs;
+          System.out.println("Daily Wage is: "+DailyWage);
        }
        else
        {
