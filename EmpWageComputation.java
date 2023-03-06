06d0bb1f7aff8a5cc1b2563e597e60963866230b
Naveen <naveengorakala814@gmail.com>
2023-03-05 17:31:49 -0800

    Add:Check Employee is Present

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,6 +4,15 @@ public class EmpWageComputation
    public static void main(String args[])
    {
       System.out.println("Welcome to Employee Wage Computation Program");
-    
+       Random rand = new Random();
+       int Attendance= rand.nextInt(2);
+       if(Attendance==1)
+       {
+          System.out.println("Employee is Present");
+       }
+       else
+       {
+          System.out.println("Employee is Absent");
+       }
    }
 }
