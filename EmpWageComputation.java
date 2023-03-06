1d2381953a2b2355eb566060dbb6ecf269f8b78b
Naveen <naveengorakala814@gmail.com>
2023-03-05 17:22:42 -0800

    Add: Welcome Message

---
+++ EmpWageComputation.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class EmpWageComputation
+{
+   public static void main(String args[])
+   {
+      System.out.println("Welcome to Employee Wage Computation Program");
+    
+   }
+}
