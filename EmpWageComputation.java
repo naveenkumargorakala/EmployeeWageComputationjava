9d7aae5f599dff6baeeaa169d67e5367ac5f2f0d
Naveen <naveengorakala814@gmail.com>
2023-03-05 17:41:58 -0800

    Add:Parttime Employee Wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,23 +1,31 @@
 import java.util.*;
-public class EmpWage
+import java.util.Random;
+public class EmpWageComputation
 {
    public static void main(String args[])
-   {  
+   {
       int DailyWage,WagePhr=20,empHrs;
       System.out.println("Welcome to Employee Wage Computation Program");
-       Random rand = new Random();
-       int Attendance= rand.nextInt(2);
-       if(Attendance==1)
-       {
-          System.out.println("Employee is Present");
-          empHrs=8;
-          System.out.println("Employee is Present");
-          DailyWage=WagePhr*empHrs;
-          System.out.println("Daily Wage is: "+DailyWage);
-       }
-       else
-       {
-          System.out.println("Employee is Absent");
-       }
+      Random rand = new Random();
+      int Attendance= rand.nextInt(3);
+      if(Attendance==1)
+      {
+         empHrs=8;
+         System.out.println("Employee is Present");
+         DailyWage=WagePhr*empHrs;
+         System.out.println("Daily Wage is: "+DailyWage);
+         
+      }
+      else if(Attendance==2)
+      {
+         empHrs=4;
+         System.out.println("PartTime");
+         DailyWage=WagePhr*empHrs;
+         System.out.println("PartTime Wage is: "+DailyWage);
+      }
+      else
+      {
+         System.out.println("Employee is Absent");
+      }
    }
 }
