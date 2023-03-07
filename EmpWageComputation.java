d3f10e7a6229075d69961c03385d9b1b4a915900
Naveen <naveengorakala814@gmail.com>
2023-03-06 19:19:52 -0800

    Add:Working Days are 20 per month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,30 +2,48 @@ import java.util.*;
 import java.util.Random;
 public class EmpWageComputation
 {
+     
    public static void main(String args[])
    {
-      int DailyWage,WagePhr=20,empHrs;
+      int DailyWage=0,WagePhr=20,empHrs;
+      int WorkingDays=20,c=0,k=0;
+      int DailyParttime=0;
       System.out.println("Welcome to Employee Wage Computation Program");
       Random rand = new Random();
-      int Attendance= rand.nextInt(3);
-      if(Attendance==1)
+      int Days=0, Hrs=0;
+     
+      for(int i=0;i<WorkingDays;i++)
       {
-         empHrs=8;
-         System.out.println("Employee is Present");
-         DailyWage=WagePhr*empHrs;
-         System.out.println("Daily Wage is: "+DailyWage);
+
+         int Attendance = rand.nextInt(3);
+         switch(Attendance)
+         {
+            case 1:
+               empHrs=8;
+               System.out.println("Employee is Present");
+               DailyWage=WagePhr*empHrs;
+               System.out.println("Daily Wage is: "+DailyWage);
+               c++;
+               Days++;
+               Hrs+=empHrs;
+            break;
+            case 2:
+               empHrs=4;
+               System.out.println("PartTime");
+               DailyParttime=WagePhr*empHrs;
+               System.out.println("PartTime Wage is: "+DailyParttime);
+               k++;
+               Days++;
+               Hrs+=empHrs;
+            break;
+            case 0:
          
-      }
-      else if(Attendance==2)
-      {
-         empHrs=4;
-         System.out.println("PartTime");
-         DailyWage=WagePhr*empHrs;
-         System.out.println("PartTime Wage is: "+DailyWage);
-      }
-      else
-      {
-         System.out.println("Employee is Absent");
-      }
+               System.out.println("Employee is Absent");
+               Days++;
+            break;
+         }
+   }
+     int TotalSalary=(DailyWage*c)+(k*DailyParttime) ;
+     System.out.println("Salary is: "+TotalSalary);  
    }
 }
