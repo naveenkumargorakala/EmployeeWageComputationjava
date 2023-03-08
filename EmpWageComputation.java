2f6c903292d8a9919096f1d8218852803f812436
Naveen <naveengorakala814@gmail.com>
2023-03-07 21:19:11 -0800

    Refactor: Refactor the code by using class and method

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,16 +2,14 @@ import java.util.*;
 import java.util.Random;
 public class EmpWageComputation
 {
-     
-   public static void main(String args[])
-   {
+  
+      public static void Employee()
+      {
       int DailyWage=0,WagePhr=20,empHrs;
       int WorkingDays=20,c=0,k=0;
       int DailyParttime=0;
-      System.out.println("Welcome to Employee Wage Computation Program");
       Random rand = new Random();
       int Days=0, Hrs=0;
-     
       for(int i=0;i<WorkingDays;i++)
       {
 
@@ -23,7 +21,6 @@ public class EmpWageComputation
       }
       else
       {
-
          int Attendance = rand.nextInt(3);
          switch(Attendance)
          {
@@ -51,10 +48,18 @@ public class EmpWageComputation
                Days++;
             break;
          }
+       System.out.println("Hours is: "+Hrs); 
       }
    }
-     int TotalSalary=(DailyWage*c)+(k*DailyParttime) ;
+     int TotalSalary=Hrs*20;
      System.out.println("Salary is: "+TotalSalary);  
       System.out.println("Hours is: "+Hrs);  
    }
+
+public static void main(String args[])
+{
+   System.out.println("Welcome to Employee Wage Computation Program");
+   EmpWageComputation Emp = new EmpWageComputation();
+   Emp.Employee();
+}
 }
