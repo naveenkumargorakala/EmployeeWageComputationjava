e343eb3fc66d8a0d5dd2d2dd5d7d5b7d43dba9dc
Naveen <naveengorakala814@gmail.com>
2023-03-10 16:47:13 +0530

    Add: Various companies with different Parameters

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,65 +1,56 @@
-import java.util.*;
+package com.BridgeLabz.basics;
 import java.util.Random;
-public class EmpWageComputation
-{
-  
-      public static void Employee()
-      {
-      int DailyWage=0,WagePhr=20,empHrs;
-      int WorkingDays=20,c=0,k=0;
-      int DailyParttime=0;
+import java.util.Scanner;
+
+public class EmpWageComputation {
+    int DailyWage=0,EmpHrs=0,c=0,k=0;
+    int WorkingHrs=0;
+  public void employeesalary(int WagePerHr,int WorkingDays)
+  {
       Random rand = new Random();
-      int Days=0, Hrs=0;
       for(int i=0;i<WorkingDays;i++)
       {
-
-      if(Days==20 || Hrs==100)
-      { 
-         System.out.println("Working Days are completed or 100 Working hours are Reached");
-         break;
-  
+          int attendance = rand.nextInt(3);
+          switch(attendance)
+          {
+              case 1:
+                  EmpHrs=8;
+                  DailyWage=WagePerHr*EmpHrs;
+                  System.out.println("Employee Present"+DailyWage);
+                  WorkingHrs+=8;
+                  c++;
+                  break;
+              case 2:
+                  EmpHrs=4;
+                  DailyWage=EmpHrs*WagePerHr;
+                  System.out.println("PartTime Wage"+DailyWage);
+                  WorkingHrs+=4;
+                  k++;
+                  break;
+              case 0:
+                  System.out.println("Employee is Absent");
+                  break;
+          }
       }
-      else
-      {
-         int Attendance = rand.nextInt(3);
-         switch(Attendance)
-         {
-            case 1:
-               empHrs=8;
-               System.out.println("Employee is Present");
-               DailyWage=WagePhr*empHrs;
-               System.out.println("Daily Wage is: "+DailyWage);
-               c++;
-               Days++;
-               Hrs+=empHrs;
-            break;
-            case 2:
-               empHrs=4;
-               System.out.println("PartTime");
-               DailyParttime=WagePhr*empHrs;
-               System.out.println("PartTime Wage is: "+DailyParttime);
-               k++;
-               Days++;
-               Hrs+=empHrs;
-            break;
-            case 0:
-         
-               System.out.println("Employee is Absent");
-               Days++;
-            break;
-         }
-       System.out.println("Hours is: "+Hrs); 
-      }
-   }
-     int TotalSalary=Hrs*20;
-     System.out.println("Salary is: "+TotalSalary);  
-      System.out.println("Hours is: "+Hrs);  
-   }
+      int TotalSalary=WorkingHrs*WagePerHr;
+      System.out.println("TotalSalary: "+TotalSalary);
+      System.out.println("FullTime Days: "+c);
+      System.out.println("PartTime Days: "+k);
+      System.out.println("Total Working hours are: "+WorkinghHrs);
+  }
 
-public static void main(String args[])
-{
-   System.out.println("Welcome to Employee Wage Computation Program");
-   EmpWageComputation Emp = new EmpWageComputation();
-   Emp.Employee();
-}
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+        EmpWageComputation Company1 = new EmpWageComputation();
+        EmpWageComputation Company2 = new EmpWageComputation();
+        Scanner input = new Scanner(System.in);
+        System.out.println("WagePerHr is: ");
+        int WagePerHr = input.nextInt();
+        System.out.println("No.of Working Days: ");
+        int WorkingDays = input.nextInt();
+        System.out.println("Company1 employee salary: ");
+        Company1.employeesalary( WagePerHr, WorkingDays);
+        System.out.println("Company2 Employee Salary:  ");
+        Company2.employeesalary(80,22);
+    }
 }
